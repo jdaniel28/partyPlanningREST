ab07ff07f2fc6a2c9438b9a18241bbeae4748d64
jdaniel28 <sjosephdaniel28@gmail.com>
2022-02-15 16:55:04 +0530

    Created Spring Boot application for Party Planning REST API

---
+++ src/test/java/com/cts/party/PartyPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
